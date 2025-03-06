d50dad94ca04ab2f8195bd5a584b743440346fd8
Alexander <aosipov@inno.tech>
2025-03-06 16:25:12 +0400

    FIRST

--- pages/+Head.tsx
+++ pages/+Head.tsx
@@ -7,6 +7,12 @@ export default function HeadDefault() {
   return (
     <>
       <link rel="icon" href={logoUrl} />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Inter:opsz,wght@14..32,100..900&display=swap"
+        rel="stylesheet"
+      />
     </>
   );
 }
